1c1b0c06dbc2ebc06f71f8db6f383caa58026e8d
douglaswe <79637913+douglaswe@users.noreply.github.com>
2022-08-22 05:17:24 -0300

    Delete Composicao.java

--- Engenharia3/Estrategia/pattern/Composicao.java
+++
@@ -1,9 +0,0 @@
-package patterns;
-
-public class Composicao {  
-    public static void main(String[] args) {  
-Carro c1 = new Carro();  
-        Part p = new Part();  
-        c1.getData(p.getMarcaName());  
-        }  
-  }  
